8291d6f4f69b05a83e1f73948e00414753e29aa1
Jonathan Kaufman <jondakauf@gmail.com>
2015-09-15 17:54:38 -0400

    add endpoints for demo

--- server/routes.js
+++ server/routes.js
@@ -74,4 +74,110 @@ function _doGet(url, qs, res) {
   });
 }
 
+// ----------------------------------------------------------------------
+// ---------------------- Start Endpoints for Demo ----------------------
+// ----------------------------------------------------------------------
+
+/** Get stocks with positive change from a list of symbols, sorted by change */
+router.get('/demo/positive', (req, res) => {
+  var symbols = req.query.symbols;
+  var {client_id, client_secret, url} = vcapServices.stockPrice.credentials;
+  request.getAsync({url: url + '/markets/quote', qs: {client_id: client_id, symbols: symbols}}).then(([response, body]) => {
+    var parsedResponse = typeof body === 'string' ? JSON.parse(body) : body;
+    res.json(parsedResponse.filter(s => s.change > 0)
+      .sort((s1, s2) => s2.change - s1.change)
+      .map(s => ({
+        change: s.change,
+        symbol: s.symbol,
+        description: s.description,
+        value: s.value
+      })));
+  }).catch(e => {
+    res.status(500);
+    res.json(e);
+  });
+});
+
+/** Get stocks with negative change from a list of symbols, sorted by change */
+router.get('/demo/negative', (req, res) => {
+  var symbols = req.query.symbols;
+  var {client_id, client_secret, url} = vcapServices.stockPrice.credentials;
+  request.getAsync({url: url + '/markets/quote', qs: {client_id: client_id, symbols: symbols}}).then(([response, body]) => {
+    var parsedResponse = typeof body === 'string' ? JSON.parse(body) : body;
+    res.json(parsedResponse.filter(s => s.change < 0)
+      .sort((s1, s2) => s1.change - s2.change)
+      .map(s => ({
+        change: s.change,
+        symbol: s.symbol,
+        description: s.description,
+        value: s.value
+      })));
+  }).catch(e => {
+    res.status(500);
+    res.json(e);
+  });
+});
+
+/** Get an array of entities w/ average sentiment sorted by count about a company */
+router.get('/demo/entities', (req, res) => {
+  var symbol = req.query.symbol;
+  var {client_id, client_secret, url} = vcapServices.stockSentiment.credentials;
+    request.getAsync({url: url + '/news/find', qs: {client_id: client_id, symbol: symbol}}).then(([response, body]) => {
+    var parsedResponse = typeof body === 'string' ? JSON.parse(body) : body;
+    // step 1: map
+    var entityMap = {};
+    for (var article of parsedResponse.news) {
+      for (var {score, sentiment, text} of article.entities) {
+        var cased = text.replace(/\w*/g, txt =>
+          txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+        );
+        var multiplier;
+        if (sentiment === 'positive') {
+          multiplier = 1;
+        } else if (sentiment === 'negative') {
+          multiplier = -1;
+        } else if (sentiment === 'neutral') {
+          multiplier = 0;
+        }
+        if (!entityMap[cased]) {
+          entityMap[cased] = [];
+        }
+        entityMap[cased].push(multiplier * score);
+      }
+    }
+    // step 2: reduce
+    var entities = [];
+    for (var text in entityMap) {
+      var __entities = entityMap[text];
+      entities.push({
+        text: text,
+        count: __entities.length,
+        averageSentiment: __entities.reduce((s, it) => s+it, 0)/__entities.length
+      })
+    }
+    // step 3: party
+    res.json(entities.sort((e1, e2) => e2.count - e1.count));
+  }).catch(e => {
+    res.status(500);
+    res.json(e);
+  });
+});
+
+/** Get an array of articles + relations for a company */
+router.get('/demo/articles', (req, res) => {
+  var symbol = req.query.symbol;
+  var {client_id, client_secret, url} = vcapServices.stockSentiment.credentials;
+    request.getAsync({url: url + '/news/find', qs: {client_id: client_id, symbol: symbol}}).then(([response, body]) => {
+    var parsedResponse = typeof body === 'string' ? JSON.parse(body) : body;
+    res.json(parsedResponse.news.map(n => ({
+      title: n.title,
+      url: n.url,
+      relations: n.relations
+    })));
+  }).catch(e => {
+    res.status(500);
+    res.json(e);
+  });
+});
+
 export default router;
