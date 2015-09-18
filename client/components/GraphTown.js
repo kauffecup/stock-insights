b1b24e5181888c1ce1f682a16a6093fa0d8d6b0b
Jonathan Kaufman <jondakauf@gmail.com>
2015-09-18 13:15:00 -0400

    resize graph when window resizes, fix weird behavior when first showing the graph

--- client/components/GraphTown.js
+++ client/components/GraphTown.js
@@ -18,6 +18,33 @@ import React       from 'react';
 import {LineChart} from 'react-d3/linechart';
 
 export default class GraphTown extends React.Component {
+  /** Initialize the height to be 200 and width to 1500? this gets overriden immediately */
+  constructor(props) {
+    super(props);
+    this.state = {height: 200, width: 1500};
+  }
+
+  /** render town */
+  render() {
+    return (
+      <div className="graph-town">
+        <LineChart data={this.adaptData()}
+          xAxisTickInterval={{unit: 'day', interval: 7}}
+          yAxisTickCount={4}
+          width={this.state.width}
+          height={this.state.height}
+          legend={true}
+          viewBoxObject={{
+            x: 0,
+            y: 0,
+            height: this.state.height,
+            width: this.state.width
+          }} />
+      </div>
+    );
+  }
+
+  /** Adapt the histories map in to an array just the way we want it */
   adaptData() {
     var myData = [];
     for (var symbol in this.props.histories) {
@@ -35,22 +62,26 @@ export default class GraphTown extends React.Component {
     return myData;
   }
 
-  render() {
-    return (
-      <div className="graph-town">
-        <LineChart data={this.adaptData()}
-          xAxisTickInterval={{unit: 'day', interval: 7}}
-          yAxisTickCount={4}
-          width={React.findDOMNode(this).clientWidth}
-          height={React.findDOMNode(this).clientHeight}
-          legend={true}
-          viewBoxObject={{
-            x: 0,
-            y: 0,
-            height: React.findDOMNode(this).clientHeight,
-            width: React.findDOMNode(this).clientWidth
-          }} />
-      </div>
-    );
+  /** Measure our dom node and set the state */
+  updateSize() {
+    this.setState({height: React.findDOMNode(this).clientHeight, width: React.findDOMNode(this).clientWidth});
+  }
+  /** When we mount, intialize resize handler */
+  componentDidMount() {
+    this.handleResize = (e => this._handleResize(e));
+    window.addEventListener('resize', this.handleResize);
+    this.updateSize();
+  }
+  /** When we're peacing out, remove the handler */
+  componentWillUnmount() {
+    window.removeEventListener('resize', this.handleResize);
+  }
+    /** On a debounce, adjust the size of our graph area and then update the chart */
+  _handleResize(e) {
+    this.__resizeTimeout && clearTimeout(this.__resizeTimeout);
+    this.__resizeTimeout = setTimeout(() => {
+      this.updateSize();
+      delete this.__resizeTimeout;
+    }, 200);
   }
 }
